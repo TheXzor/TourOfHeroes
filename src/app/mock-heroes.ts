8686072136f7009250fdd487238da5ba756d5211
Desktop-Xzor <martin_hjelland@hotmail.com>
2022-10-25 12:49:16 +0200

    Added display of list of heroes added from array. Added array of heroes in its own, separate file. Added ability to click hero in list to display that hero's details.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Cenarius' },
+    { id: 13, name: 'Sargeras' },
+    { id: 14, name: 'Malfurion' },
+    { id: 15, name: 'Illidan' },
+    { id: 16, name: 'Ragnaros' },
+    { id: 17, name: 'Valeera' },
+    { id: 18, name: 'Magni' },
+    { id: 19, name: 'Gallywix' },
+    { id: 20, name: 'Anduin' },
+]
